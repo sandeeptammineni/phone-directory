b2de1f4d398b5d32cb05cabee9339d0941eb4f7e
Sandeep <sandeeptammineni24@gmail.com>
2023-11-21 14:53:38 +0530

    Rendered Subscribers dynamically

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import React, { Component } from "react";
-import Header from "./header";
+import React, { Component } from 'react';
+import Header from './Header.js';
+import './App.css';
 
 class App extends Component {
-  render(){
-  return (
+  render() {
+    let subscribers=[
+    {
+      id:1,
+      name:'Shilpa',
+      phone:'8888888888'
+    },
+    {
+      id:2,
+      name:'Sristi',
+      phone:'9999999999'
+    }
+  ]
+    return (
       <div>
-      <Header/>,
-      <h2>Contact</h2></div>
-  )
-}
+        <Header />
+        <div className="component-body-container">
+          <button className="custom-btn add-btn">Add</button>
+
+          <div className="grid-container heading-container">
+            <span className="grid-item name-heading">Name</span>
+            <span className="grid-item phone-heading">Phone</span>
+          </div>
+          {
+            subscribers.map(sub => {
+              return <div key={sub.id} className="grid-container ">
+              <span className="grid-item ">{sub.name}</span>
+              <span className="grid-item ">{sub.phone}</span>
+            </div>
+            })
+          }
+        </div>
+      </div>
+    );
   }
+}
 
 export default App;
+
--- src/header.js
+++ src/header.js
@@ -1,9 +1,9 @@
 import React from "react";
+import './Header.css'
 
 const Header=function(){
-    const headerstyle={color:'white',backgroundColor:'black',textAlign:'center',padding:20}
     return(
-        <h1 style={headerstyle}>Phone Directory</h1>
+        <h1 className="header">Phone Directory</h1>
     )
 }
 
